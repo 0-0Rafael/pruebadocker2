709e41c48a84c10246d62ea44a86311c61657155
Rafael Ariel Custodio Guillen <107286551+0-0Rafael@users.noreply.github.com>
2023-11-03 19:30:52 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba docker 2</title>
+</head>
+<body>
+
+    <h3>Practica Docker Compose/php/Mysql, Estudiante: Rafael Ariel Custodio Guillen <b>2021-2054</b></h3>
+
+    <img src="shy.jpeg" alt="">
+    
+<?php
+
+        $hosting = getenv('MYSQL_HOST'); 
+        $usernamesql = getenv('MYSQL_USER');
+        $passwordsql = getenv('MYSQL_PASSWORD');
+        $database = getenv('MYSQL_DATABASE');
+
+
+        // En esta parte se esta intentando realizar la conexion a la bd
+        $connetao = new mysqli($hosting, $usernamesql, $passwordsql, $database);
+
+        if ($connetao->connect_error) {
+        die("Hubo un error de conexión: " . $conne->connect_error);
+        }
+
+        echo "La conexion fue realizada con exito";
+?>
+
+</body>
+</html>
